e6adbb829af0b07b227dc26edb7ad297796b6737
Sciencealone <info@markovsky.ru>
2017-02-09 22:26:11 +0300

    Update with export buttons and select boxes

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 import pandas as pd
 import pymssql
 import re
+from bs4 import BeautifulSoup
 
 # Visible rows limit
 rows_limit = 5000
@@ -19,21 +20,25 @@ app = Flask(__name__)
 @app.route("/")
 def show_index():
     # Prepare html code for the table
-    html = df.head(rows_limit).to_html(classes = 'example', border = 0, index = False)
+    html = df.head(rows_limit).to_html(classes = 'display example', border = 0, index = False)
+    
+    # Fix some formatting issues
     html = re.sub(' example', '" id = "example', html)
+    html = re.sub(' style="text-align: right;"', '', html)
+    
+    # Prepare a fake footer for the select boxes
+    soup = BeautifulSoup(html, "lxml")
+    thead = str(soup.find('thead').findChildren()[0])
+    html = re.sub(r'</thead>', r'</thead><tfoot>' + thead + '</tfoot>', html)
+    
     return render_template('index.html', table = html)
 
 if __name__ == "__main__":
     
     # Retreive MS SQL data
     conn = pymssql.connect(server, user, password, base)
-    df = pd.read_sql("select * from consumer_complaints", con = conn)
+    df = pd.read_sql("select * from Camera", con = conn)
     conn.close()
-    
-    # Remove empty or equal filled columns
-    df.drop('consumer_complaint_narrative', axis = 1, inplace = True)
-    df.drop('company_public_response', axis = 1, inplace = True)
-    df.drop('consumer_consent_provided', axis = 1, inplace = True)
     
     # Run the app with enabled debug
     app.run(debug = True)
